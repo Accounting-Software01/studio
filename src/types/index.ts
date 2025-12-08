be2f89691dbd28d4d793d5e760b4886bc887bf79
M. Sageer Hamisu <msabdhallah@gmail.com>
2025-12-08 14:36:33 +0000

    okay continue

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,42 +1,44 @@
 
+
 export interface InvoiceItem {
   id: string | number;
   productName: string;
-  baseQty: {
-    actual: number;
-    billed: number;
-  };
-  altQty: string;
-  rate: number;
-  per: string;
-  disc?: number;
-  amount: number;
+  quantity: number;
+  unitPrice: number;
+  totalPrice: number;
 }
 
 export interface CompanyDetails {
   logoUrl?: string;
   name: string;
   address: string;
+  phone: string;
+  email: string;
+  website: string;
 }
 
-export interface Customer {
-  name: string;
+export interface LedgerAccount {
+    code: string;
+    name: string;
+    balance: number;
 }
 
 export interface Invoice {
   id: string | number;
   invoiceNumber: string;
-  issueDate: string; // ISO date string
-  deliveryNote: string;
-  despatchDocumentNo: string;
-  destination: string;
+  issueDate: Date;
+  dueDate: Date;
+  status: 'Paid' | 'Unpaid' | 'Overdue';
   companyDetails: CompanyDetails;
-  customer: Customer;
+  customerName: string;
+  customerAddress: string;
+  customerEmail: string;
+  customerId: string;
   items: InvoiceItem[];
   subTotal: number;
-  vatAmount?: number;
+  discountAmount?: number;
+  taxAmount?: number;
+  taxRate?: number;
   totalAmount: number;
-  previousBalance?: number;
-  newBalance?: number;
-  narration?: string | null;
+  notes?: string | null;
 }
